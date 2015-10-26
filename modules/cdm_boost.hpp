46d6f05747b8783ba6ebd9235e84b23c4f8a0c83
TyRoXx <tyroxxxx@gmail.com>
2015-10-26 21:13:37 +0100

    remove Boost log buffering because we do not print that to the console anymore

--- modules/cdm_boost.hpp
+++ modules/cdm_boost.hpp
@@ -6,17 +6,6 @@
 #include <silicium/sink/iterator_sink.hpp>
 #include <boost/lexical_cast.hpp>
 
-// Boost is so large that the output of cp exceeds the 4 MB log limit of travis
-// which aborts the build job then.
-// On Windows, the console is so slow (especially in Virtualbox) that we want
-// to avoid the megabytes of output from the Boost build process.
-// TODO: Buffer the output in memory and save it somewhere if something fails.
-#if CDM_TESTS_RUNNING_ON_TRAVIS_CI || defined(_WIN32)
-#define CDM_AVOID_CONSOLE_OUTPUT 1
-#else
-#define CDM_AVOID_CONSOLE_OUTPUT 0
-#endif
-
 namespace cdm
 {
 	struct boost_paths
@@ -32,14 +21,7 @@ namespace cdm
 		if (!ventura::file_exists(module_in_cache, Si::throw_))
 		{
 			ventura::absolute_path const copy_of_boost = temporary / "src";
-			ventura::copy_recursively(source, copy_of_boost,
-#if CDM_AVOID_CONSOLE_OUTPUT
-			                          nullptr
-#else
-			                          &output
-#endif
-			                          ,
-			                          Si::throw_);
+			ventura::copy_recursively(source, copy_of_boost, &output, Si::throw_);
 
 			ventura::create_directories(module_in_cache, Si::throw_);
 
@@ -60,10 +42,6 @@ namespace cdm
 			}
 
 			{
-#if CDM_AVOID_CONSOLE_OUTPUT
-				std::vector<char> output_buffer;
-				auto buffering = Si::Sink<char, Si::success>::erase(Si::make_container_sink(output_buffer));
-#endif
 				std::vector<Si::os_string> arguments;
 				arguments.emplace_back(SILICIUM_OS_STR("install"));
 				{
@@ -88,22 +66,9 @@ namespace cdm
 				                                                    ".exe"
 #endif
 				                                    ,
-				                                    arguments, copy_of_boost,
-#if CDM_AVOID_CONSOLE_OUTPUT
-				                                    buffering
-#else
-				                                    output
-#endif
-				                                    ).get();
+				                                    arguments, copy_of_boost, output).get();
 				if (rc != 0)
 				{
-#if CDM_AVOID_CONSOLE_OUTPUT
-					// TODO: do not buffer more characters than necessary
-					std::size_t const max_output = 3500 * 1000;
-					std::size_t const actual_output = (std::min)(max_output, output_buffer.size());
-					char const *const end = output_buffer.data() + output_buffer.size();
-					Si::append_range(output, Si::make_iterator_range(end - actual_output, end));
-#endif
 					throw std::runtime_error("b2 failed");
 				}
 			}
